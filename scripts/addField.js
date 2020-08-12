b0b16e41765dda5534c4e11b3749d622c874bf4c
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-08-12 19:24:04 -0300

    dia 03 - pag classes, inicio do js, fim da estilização

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+let btn = document.querySelector('#add-time')
+btn.addEventListener('click', cloneField)
+
+function cloneField() {
+  //duplicar os campos
+  const newFieldsContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+  //pegar os campos?
+  const fields = newFieldsContainer.querySelectorAll('input')
+
+  //para cada campo limpar
+  fields.forEach(field => console.log(field))
+  //pegar o field do momento e limpa ele
+    field.value = ""
+  
+
+
+  //inserir os dados duplicados na pagina
+  document.querySelector('#schedule-items').appendChild(newFieldsContainer)
+}
